6c1afd2bbb3674d479123fafd66054edaf4a2c01
Henry Richardson <hrichardson83@gmail.com>
2019-04-02 19:19:37 -0400

    database connection made to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
-
+console.log('working');
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('connected');
+// });
 const app = express();
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
