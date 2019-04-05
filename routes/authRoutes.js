4342347e0cb95dff73a088282133dd4aae1221c1
Henry Richardson <hrichardson83@gmail.com>
2019-04-05 16:47:37 -0400

    testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,8 +8,7 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", console.log("testing"));
-  // passport.authenticate("google")
+  app.get("/auth/google/callback", passport.authenticate("google"));
   app.get("/api/logout", (req, res) => {
     req.logout();
     res.send(req.user);
