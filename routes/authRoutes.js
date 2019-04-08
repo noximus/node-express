375e2929cdb9a78a41bebfa55b24d4264b7921f3
Henry Richardson <hrichardson83@gmail.com>
2019-04-07 22:51:11 -0400

    heroku testing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,9 @@ const passport = require("passport");
 
 module.exports = app => {
   app.get("/", (req, res) => {
-    hi: "buddy"; 
+    hi: "buddy";
   });
-  
+
   app.get(
     "/auth/google",
     passport.authenticate("google", {
@@ -12,11 +12,17 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
 
   app.get("/api/current_user", (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,7 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
-
+      console.log(existingUser);
       if (existingUser) {
         return done(null, existingUser);
       }
